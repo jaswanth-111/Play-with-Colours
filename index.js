28f78c54dc07cfc45efd7d357b0477ca949b2ac8
MEESALA JASWANTH <102941829+jaswanth-111@users.noreply.github.com>
2023-03-04 20:40:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function fun()
+{
+    var c=document.getElementById("2").value;
+    document.getElementById("4").style.color =c;
+    document.getElementById("5").style.color =c;
+}
+
+function fun1()
+{
+    var c=document.getElementById("3").value;
+    document.getElementById("1").style.backgroundColor= c;
+}
